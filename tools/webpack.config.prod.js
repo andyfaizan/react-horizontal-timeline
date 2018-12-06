4875a33bbe7713fc6fbcdf04b6e655c21d12f5fb
Ainuddin Faizan <andyfaizan@gmail.com>
2018-12-06 16:31:58 +0100

    add svgr to prod webpack

--- tools/webpack.config.prod.js
+++ tools/webpack.config.prod.js
@@ -2,48 +2,50 @@
  * WEBPACK CONFIG FOR PRODUCTION USE
  */
 
-const path = require('path');
-const sourcePath = path.join(__dirname, '..', 'src');
-const distPath = path.join(__dirname, '..', 'dist');
-const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const path = require("path");
+const sourcePath = path.join(__dirname, "..", "src");
+const distPath = path.join(__dirname, "..", "dist");
+const webpack = require("webpack");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 const rules = [
   // Babel enables the use of ES6 today by transpiling your ES6 JavaScript into equivalent ES5
   // source that is actually delivered to the end user browser.
   {
     test: /\.jsx?$/,
-    use: [ 'babel-loader' ],
+    use: ["babel-loader"],
     include: sourcePath
   },
+  {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"]
+  }
 ];
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   optimization: {
     minimizer: [new UglifyJSPlugin()]
   },
-  entry: [
-    path.join(sourcePath, 'Components/HorizontalTimeline.jsx')
-  ],
+  entry: [path.join(sourcePath, "Components/HorizontalTimeline.jsx")],
   // If you pass an array - the modules are loaded on startup. The last one is exported.
   output: {
     path: distPath,
-    filename: 'react-horizontal-timeline.js',
-    libraryTarget: 'commonjs2'
+    filename: "react-horizontal-timeline.js",
+    libraryTarget: "commonjs2"
   },
   // Array of file extensions used to resolve modules.
   resolve: {
-    extensions: [ '.js', '.jsx' ]
+    extensions: [".js", ".jsx"]
   },
   // External dependencies don't need to be in our own dist
   externals: [
-    'color',
-    'radium',
-    'react',
+    "color",
+    "radium",
+    "react",
     /^react-icons\b/,
-    'react-motion',
-    'react-dimensions'
+    "react-motion",
+    "react-dimensions"
   ],
   module: {
     rules: rules
